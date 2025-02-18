5b18e65f26c73e9b9d2e58d1f6bdbc8b15a9829d
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-02-18 22:01:07 +0530

    update bank-webhook/src/index.ts

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,4 +1,5 @@
 
+import prisma from "@repo/db/client"
 import express, { Request, Response } from "express"
 
 const app = express() 
@@ -6,8 +7,42 @@ const PORT = 3003
 
 app.use(express.json())
 
-app.get('/hdfcWebhook', (req: Request, res: Response) => {
+app.get('/hdfcWebhook', async (req: Request, res: Response): Promise<Response | any> => {
   // do zod validation here
+  const paymentInformation = {
+    token: req.body.token,
+    userId: req.body.userId,
+    amount: req.body.amount
+  }
+
+  try {
+    await prisma.$transaction([
+      prisma.balance.update({
+        where: {
+          userId: paymentInformation.userId
+        },
+        data: {
+          amount: {
+            increment: Number(paymentInformation.amount)
+          }
+        }
+      }),
+      prisma.onRampTransactions.update({
+        where: {
+          token: paymentInformation.token
+        },
+        data: {
+          status: "Success"
+        }
+      })
+    ])
+
+    return res.json({message: "captured"})
+  }
+  catch(err) {
+    console.log(err)
+    return res.status(411).json({message: "Error while processing webhook"})
+  }
 })
 
 app.listen(PORT, () => {
