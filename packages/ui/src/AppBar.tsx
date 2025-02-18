bd62cdb4dbf52ad6d04bcbe6611f7fecc5b7bdaf
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-02-18 21:00:34 +0530

    updated classNames

--- packages/ui/src/AppBar.tsx
+++ packages/ui/src/AppBar.tsx
@@ -11,11 +11,11 @@ interface AppBarProps {
 
 export const AppBar = ({user, onSignIn, onSignOut}: AppBarProps): JSX.Element => {
 
-  return <div className="flex justify-between shadow-sm px-10 py-3 items-center">
-    <div className="text-3xl font-extrabold pb-1 text-gray-800 hover:text-gray-700 cursor-pointer">
+  return <div className="flex justify-between shadow-sm px-10 py-2 items-center">
+    <div className="text-2xl font-extrabold pb-1 text-gray-800 hover:text-gray-700 cursor-pointer">
       XALLET
     </div>
-    <div className="">
+    <div className="pb-1">
       <Button onClickHandler={user ? onSignOut : onSignIn}>{user ? "Logout" : "Login"}</Button>
     </div>
   </div>
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -10,6 +10,6 @@ interface ButtonProps {
 
 export const Button = ({ children, onClickHandler }: ButtonProps): JSX.Element => {
   return (
-    <button onClick={onClickHandler} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-md px-6 py-2 me-2 mb-2 cursor-pointer">{children}</button>
+    <button onClick={onClickHandler} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-6 py-2 cursor-pointer">{children}</button>
   )
 };
