50edc390c8aee38c9db1516f06c89b87ae1b2d81
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-02-18 17:25:41 +0530

    update user-app/layout.tsx

--- apps/user-app/providers.tsx
+++ apps/user-app/providers.tsx
@@ -2,11 +2,14 @@
 
 import React from "react";
 import { RecoilRoot } from "recoil";
+import { SessionProvider } from "next-auth/react"
 
 export const Providers = ({children}: {children: React.ReactNode}) => {
   return <div>
     <RecoilRoot>
-      {children}
+      <SessionProvider>
+        {children}
+      </SessionProvider>
     </RecoilRoot>
   </div>
 }
