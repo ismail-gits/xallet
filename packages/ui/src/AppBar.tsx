d24f8306fde243fb03cf40fadacfeb1e9a64504c
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-02-18 20:38:11 +0530

    adjusted frontend

--- packages/ui/src/AppBar.tsx
+++ packages/ui/src/AppBar.tsx
@@ -11,11 +11,11 @@ interface AppBarProps {
 
 export const AppBar = ({user, onSignIn, onSignOut}: AppBarProps): JSX.Element => {
 
-  return <div className="flex justify-between shadow-sm px-4 py-2 items-center">
-    <div className="text-3xl font-extrabold pb-1">
+  return <div className="flex justify-between shadow-sm px-10 py-3 items-center">
+    <div className="text-3xl font-extrabold pb-1 text-gray-800 hover:text-gray-700 cursor-pointer">
       XALLET
     </div>
-    <div>
+    <div className="">
       <Button onClickHandler={user ? onSignOut : onSignIn}>{user ? "Logout" : "Login"}</Button>
     </div>
   </div>
