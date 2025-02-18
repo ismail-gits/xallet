0aeb0b9353afedc690f8a9058a9962a9d6ab1a96
Mohammed Ismail <mohammedismail72425@gmail.com>
2025-02-18 15:40:50 +0530

    added user-app/providers.tsx

---
+++ apps/user-app/app/providers.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { RecoilRoot } from "recoil";
+
+export const Providers = ({children}: {children: React.ReactNode}) => {
+  return <div>
+    <RecoilRoot>
+      {children}
+    </RecoilRoot>
+  </div>
+}
